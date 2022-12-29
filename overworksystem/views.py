8368105d647c9ea3f43daec35bc8eec8a8911b70
elepikachu <568740695@qq.com>
2022-12-29 09:15:42 +0800

    first commit

---
+++ overworksystem/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+VERSION = '小皮的工具库 1.0.0'
+
+
+def main_view(request):
+    dic = {'ver': VERSION}
+    return render(request, 'main.html', dic)
+
+
+def update_view(request):
+    dic = {'ver': VERSION}
+    return render(request, 'update.html', dic)
+
+
+def info_view(request):
+    dic = {'ver': VERSION}
+    return render(request, 'info.html', dic)
+
+
+def food_view(request):
+    dic = {'ver': VERSION}
+    return render(request, 'food.html', dic)
